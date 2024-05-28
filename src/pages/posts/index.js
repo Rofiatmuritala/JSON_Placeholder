b15b6f255e6165468708599a9d6ce544aaa1303e
rofiatmuritala <rofiatmuritalaa@gmail.com>
2024-05-28 20:45:02 +0000

    fix

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -9,11 +9,11 @@ export default function PostLists({ posts }) {
       <h1 className="text-center text-4xl capitalize font-bold mt-2">posts</h1>
       {posts.map((post) => {
         return (
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center justify-center">
-            <div
-              key={post.id}
-              className=" flex flex-col border-1 rounded-lg p-2 m-4 justify-center items-center md:m-12 bg-gray-100"
-            >
+          <div
+            key={post.id}
+            className="grid grid-cols-1 lg:grid-cols-2 gap-4 items-center justify-center"
+          >
+            <div className=" flex flex-col border-1 rounded-lg p-2 m-4 justify-center items-center md:m-12 bg-gray-100">
               {/* when we have a child that is not an anchor tag we need to pass passHref*/}
               <Link href={`posts/${post.id}`} passHref>
                 <h1 className="mb-4 font-bold text-sky-500">
